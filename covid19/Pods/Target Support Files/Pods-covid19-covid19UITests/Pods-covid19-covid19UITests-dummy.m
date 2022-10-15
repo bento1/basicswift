8fa922478a0fa25b444b479a2067a5e76f448206
bento1 <bentotoda@naver.com>
2022-10-15 19:23:12 +0900

    covid monitorr

---
+++ covid19/Pods/Target Support Files/Pods-covid19-covid19UITests/Pods-covid19-covid19UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_covid19_covid19UITests : NSObject
+@end
+@implementation PodsDummy_Pods_covid19_covid19UITests
+@end
---
+++ covid19/covid19/ApiKey.swift
@@ -0,0 +1,12 @@
+//
+//  ApiKey.swift
+//  covid19
+//
+//  Created by Dongun Yun on 2022/10/15.
+//
+
+import Foundation
+
+struct APIKEY{
+    var apikey : String = "9DWksraIBQxmHTE8tNqCOuGv2FSVXpzJy"
+}
